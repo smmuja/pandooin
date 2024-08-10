7a21240c46bad0d2d21fe469f88331e78f5ca1e4
smmuja <smmuja27@gmail.com>
2024-08-10 15:53:36 +0700

    style(homepage) : fix position floating button

--- src/layout/component/floatingButton/index.tsx
+++ src/layout/component/floatingButton/index.tsx
@@ -1,9 +1,11 @@
 export function FloatingButton() {
   return (
     <>
-      <button className="absolute bottom-5 right-5 p-1 px-3 mx-1 text-white-smoke bg-dark-green rounded-l-full rounded-br-lg rounded-tr-md">
-        Need holiday ideas? Click here!
-      </button>
+      <div className="flex justify-end sticky bottom-5 right-5">
+        <button className="hidden sm:flex sticky bottom-5 right-5 p-1 px-3 mx-1 text-white-smoke bg-dark-green rounded-l-full rounded-br-lg rounded-tr-md">
+          Need holiday ideas? Click here!
+        </button>
+      </div>
     </>
   );
 }
