74c18bd1161a5e500d0613edbff2e0712f2a0397
smmuja <smmuja27@gmail.com>
2024-08-10 17:50:26 +0700

    style(base component): adjust background color Modal component

--- src/component/modal/index.tsx
+++ src/component/modal/index.tsx
@@ -26,12 +26,12 @@ export function Modal(props: ModalProps) {
   return (
     <>
       <div
-        className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-80 flex items-center justify-center overflow-ellipsis"
+        className="fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-65 flex items-center justify-center overflow-ellipsis inset-0"
         onClick={onClose}
       >
         <div
           className={twMerge(
-            "flex flex-col bg-white p-5 rounded-md relative w-10/12 h-3/4 max-w-full cursor-zoom-out items-center justify-center box-border",
+            "flex flex-col bg-white-paper p-5 rounded-md relative w-10/12 h-3/4 max-w-full cursor-zoom-out items-center justify-center box-border",
             className
           )}
           onClick={onClose}
