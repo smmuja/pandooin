acb8ab6a7a327b9c7b46ef8bc8551c338f6ea734
smmuja <smmuja27@gmail.com>
2024-08-10 10:35:11 +0700

    style(homepage): adjust hero background and styling

--- src/features/home/hero/index.tsx
+++ src/features/home/hero/index.tsx
@@ -1,18 +1,17 @@
 export function Hero() {
   return (
     <>
-      <img src="/assets/img/home-background.webp" alt="" />
-      <div className="text-white-paper text-center absolute top-14 left-1/4">
-        <h3 className="text-9xl font-normal font-thesignature text-yellow-tan leading-10">
+      <div className="text-white-paper text-center bg-[url('/assets/img/home-background.webp')] mt-0 h-full pt-28 ">
+        <h3 className="text-7xl xl:text-9xl font-normal font-thesignature text-yellow-tan my-0 mt-10">
           Premium Travel
         </h3>
-        <p className="font-bold text-2xl">Beyond Expectation</p>
-        <p>
+        <p className="font-bold lg:text-2xl">Beyond Expectation</p>
+        <p className="text-sm sm:text-base">
           Experience the finest that Indonesia has to offer with our curated
           selection of premium trips, ensuring comfort every step of the way
         </p>
         <a href="">
-          <button className="border-2 p-2 px-3 my-3 rounded-full font-medium border-white-paper text-white-paper hover:bg-yellow-tan hover:border-transparent">
+          <button className="text-sm sm:text-base border-2 p-2 px-3 my-3 rounded-full font-medium border-white-paper text-white-paper hover:bg-yellow-tan hover:border-transparent">
             Take Me There
           </button>
         </a>
