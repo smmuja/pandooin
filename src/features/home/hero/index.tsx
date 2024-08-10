cb4b992eea5e840550a5b1104e2e1922c16cfdd5
smmuja <smmuja27@gmail.com>
2024-08-10 18:25:52 +0700

    style(homepage): adjust hero layout and styling

--- src/features/home/hero/index.tsx
+++ src/features/home/hero/index.tsx
@@ -1,20 +1,22 @@
 export function Hero() {
   return (
     <>
-      <div className="text-white-paper text-center bg-[url('/assets/img/home-background.webp')] mt-0 h-full pt-28 ">
-        <h3 className="text-7xl xl:text-9xl font-normal font-thesignature text-yellow-tan my-0 mt-10">
-          Premium Travel
-        </h3>
-        <p className="font-bold lg:text-2xl">Beyond Expectation</p>
-        <p className="text-sm sm:text-base">
-          Experience the finest that Indonesia has to offer with our curated
-          selection of premium trips, ensuring comfort every step of the way
-        </p>
-        <a href="">
-          <button className="text-sm sm:text-base border-2 p-2 px-3 my-3 rounded-full font-medium border-white-paper text-white-paper hover:bg-yellow-tan hover:border-transparent">
-            Take Me There
-          </button>
-        </a>
+      <div className="text-white-paper text-center lg:text-left bg-[url('/assets/img/home-background.webp')] bg-top mt-0 h-screen sm:pl-4 px-1 pt-36 md:h-4/5 lg:pt-56 pb-40 sm:pt-48 ">
+        <div className="lg:w-1/2">
+          <h3 className="text-7xl xl:text-8xl font-normal font-thesignature text-yellow-tan my-0 mt-10">
+            Premium Travel
+          </h3>
+          <p className="font-bold lg:text-3xl">Beyond Expectation</p>
+          <p className="text-sm sm:text-lg lg:text-xl lg:tracking-wider lg:my-3">
+            Experience the finest that Indonesia has to offer with our curated
+            selection of premium trips, ensuring comfort every step of the way
+          </p>
+          <a href="#destination">
+            <button className="text-sm sm:text-base border sm:border-2 p-2 px-3 my-3 rounded-full font-medium border-white-paper text-white-paper hover:bg-yellow-tan hover:border-transparent">
+              Take Me There
+            </button>
+          </a>
+        </div>
       </div>
     </>
   );
